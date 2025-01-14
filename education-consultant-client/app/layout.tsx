56b46054e2246f15cfafb114481fe76aa3882629
swdrana <tctr2s@gmail.com>
2025-01-14 21:58:46 +0800

    footer fix

--- education-consultant-client/app/layout.tsx
+++ education-consultant-client/app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <MainFooter/>
       </body>
-      <MainFooter/>
     </html>
   );
 }
