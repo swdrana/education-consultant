d4197240b6df9a25dec06a047d6f8b93e5d14cc4
swdrana <tctr2s@gmail.com>
2025-02-03 02:57:04 +0800

    auth e clg and NEXTAUTH_SECRET name change

--- education-consultant-client/auth.ts
+++ education-consultant-client/auth.ts
@@ -4,7 +4,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import connectDB from "./lib/connectDB";
 import User from "./models/User";
 import bcrypt from "bcryptjs";
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
     GoogleProvider({
@@ -13,33 +12,38 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     }),
     CredentialsProvider({
       credentials: {
-        email: {},
-        password: {},
+          email: {},
+          password: {},
       },
       async authorize(credentials) {
         if (!credentials || !credentials.email || !credentials.password) {
           throw new Error("Email and Password are required");
         }
-        try {
-          const user = await User.findOne({ email: credentials?.email });
-          if (user) {
-            const isMatch = await bcrypt.compare(credentials.password as string, user.password); // Compare hashed password
-            if (isMatch) {
-              return user;
-            } else {
-              throw new Error("Email or Password is not correct");
-            }
-          } else {
-            throw new Error("User not found");
+          try {
+              const user = await User.findOne({ email: credentials?.email });
+              // console.log(user);
+              if (user) {
+                const isMatch = await bcrypt.compare(credentials.password as string, user.password); // Compare hashed password
+
+                  if (isMatch) {
+                      return user;
+                  } else {
+                      throw new Error("Email or Password is not correct");
+                  }
+              } else {
+                  throw new Error("User not found");
+              }
+          } catch (error: any) {
+              throw new Error(error);
           }
-        } catch (error: any) {
-          throw new Error(error);
-        }
       },
-    }),
+  }),
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
+      console.log('signIn Callback user: ', user); 
+      console.log('signIn Callback account: ', account); 
+      console.log('signIn Callback profile: ', profile); 
       await connectDB();
       const existingUser = await User.findOne({ email: user.email });
       if (!existingUser) {
@@ -51,12 +55,15 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
             role: "user", // Default role
           });
         } catch (error) {
+          // console.log(error);
           throw new Error("User cannot be saved to DB.");
         }
       }
       return true;
     },
     async jwt({ token, user }) {
+      console.log('JWT Callback token: ', token); 
+      console.log('JWT Callback user: ', user); 
       if (user) {
         const dbUser = await User.findOne({ email: user.email });
         token.role = dbUser?.role || "user";
@@ -64,6 +71,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       return token;
     },
     async session({ session, token }) {
+      console.log('session Callback session: ', session); 
+      console.log('session Callback token: ', token); 
       if (token) {
         session.user = {
           ...session.user,
@@ -76,16 +85,5 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET!,
   session: {
     strategy: "jwt",
-  },
-  cookies: {
-    sessionToken: {
-      name: "next-auth.session-token",
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production", // Only secure cookies in production
-      },
-    },
   },
 });
--- education-consultant-client/middleware.ts
+++ education-consultant-client/middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET! });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
   
   console.log("🔹 Token in middleware:", token);  // Token দেখতে হবে
   const { pathname } = req.nextUrl;
