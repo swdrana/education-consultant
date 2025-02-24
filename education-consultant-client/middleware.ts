d35ff1acdb447dd8c4542aff6dd18b65dd875c5c
swdrana <tctr2s@gmail.com>
2025-02-24 15:45:17 +0800

    middleware getAuth implement replace of getToken

--- education-consultant-client/middleware.ts
+++ education-consultant-client/middleware.ts
@@ -1,26 +1,33 @@
+
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("next-auth.session-token")?.value;
+  // Retrieve session directly using auth()
+  const session = await auth();
 
-  console.log("🔹 Token in middleware:", token);
-
+  console.log("🔹 Session in middleware:", session); // Check session here
   const { pathname } = req.nextUrl;
 
-  if (token) {
-    console.log("✅ User is authenticated");
+  if (session) {
+    console.log(`✅ User is authenticated: ${session.user?.email}, Role: ${session.user?.role}`);
   } else {
-    console.log("❌ No token found, user unauthenticated");
+    console.log("❌ No session found, user unauthenticated");
   }
 
   // User is authenticated and trying to access login or register page
-  if ((token && pathname === "/login") || (token && pathname === "/register")) {
+  if ((session && (pathname === "/login" || pathname === "/register"))) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   // User is trying to access dashboard but is not authenticated
-  if (pathname.startsWith("/dashboard") && !token) {
+  if (pathname.startsWith("/dashboard") && !session) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  // User has role other than 'admin'
+  if (pathname.startsWith("/dashboard") && session?.user?.role !== "admin") {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
